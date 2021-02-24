f1b7c5386f7ef803833076ba81e067cafaf9c785
Markus Günther <info@unikka.de>
2021-02-24 22:14:37 +0100

    New: Add new localisations

--- Resources/Private/Javascript/Templates/ImpersonateButton.js
+++ Resources/Private/Javascript/Templates/ImpersonateButton.js
@@ -1,11 +1,14 @@
 import {isNil} from "../Helper"
 
 const impersonateIcon = '<i class="fas fa-random icon-white"></i>'
+const localizedTooltip = !isNil(window.Typo3Neos) ?
+    window.Typo3Neos.I18n.translate('tooltip.impersonateUserButton', 'Login as this user', 'Unikka.LoginAs') :
+    'Login as this user';
 
 const ImpersonateButton = (identifier, disabled) => {
     const attributesObject = {
         'data-neos-toggle': 'tooltip',
-        'data-original-title': 'Benutzer einloggen',
+        'data-original-title': localizedTooltip,
         'data-user-identifier': identifier,
         class: 'neos-button neos-button-primary impersonate-user',
     }
