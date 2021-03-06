1bd425d9807debcec8e8b8a9f2f8bf56e7737ec5
Markus Günther <info@unikka.de>
2021-03-06 01:28:22 +0100

    Upgrade: Update @unikka/loginas-api to version 1.1.0

--- Resources/Public/LoginAsUserMenu/Plugin.js
+++ Resources/Public/LoginAsUserMenu/Plugin.js
@@ -86,6 +86,17 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "../../../node_modules/@unikka/loginas-api/dist/index.js":
+/*!**********************************************************************************************************************************!*\
+  !*** /Volumes/Development/Playground/neos-70/DistributionPackages/Unikka.LoginAs/node_modules/@unikka/loginas-api/dist/index.js ***!
+  \**********************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+(()=>{"use strict";var e={d:(t,r)=>{for(var n in r)e.o(r,n)&&!e.o(t,n)&&Object.defineProperty(t,n,{enumerable:!0,get:r[n]})},o:(e,t)=>Object.prototype.hasOwnProperty.call(e,t),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},t={};e.r(t),e.d(t,{default:()=>s});const r=function(e){return null==e};function n(e,t,r,n,a,o,i){try{var s=e[o](i),u=s.value}catch(e){return void r(e)}s.done?t(u):Promise.resolve(u).then(n,a)}function a(e){return function(){var t=this,r=arguments;return new Promise((function(a,o){var i=e.apply(t,r);function s(e){n(i,a,o,s,u,"next",e)}function u(e){n(i,a,o,s,u,"throw",e)}s(void 0)}))}}function o(e){throw new TypeError('"'+e+'" is read-only')}function i(e,t){for(var r=0;r<t.length;r++){var n=t[r];n.enumerable=n.enumerable||!1,n.configurable=!0,"value"in n&&(n.writable=!0),Object.defineProperty(e,n.key,n)}}const s=function(){function e(t,n){if(function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,e),r(t)){var a="Tried to create API service without a base uri. ";a+=(o("pathError"),"Please initialize the API service with a base path "),a+=(o("pathError"),'like "/neos/impersonate/"'),console.error(a)}if(this._basePath=t,r(n)){var i="Tried to create API service without a CSFR ";i+=(o("csrfError"),"token. Please initialize the API service with a token"),console.error(i)}this._csrfToken=n}var t,n,s,u,c;return t=e,(n=[{key:"callUserChange",value:(c=a(regeneratorRuntime.mark((function e(t){var r,n;return regeneratorRuntime.wrap((function(e){for(;;)switch(e.prev=e.next){case 0:return r={user:t,format:"json"},e.next=3,fetch(this._basePath+"user-change",{method:"POST",credentials:"include",headers:this._getHeader(),body:JSON.stringify(r)});case 3:return n=e.sent,e.next=6,n.json();case 6:return e.abrupt("return",e.sent);case 7:case"end":return e.stop()}}),e,this)}))),function(e){return c.apply(this,arguments)})},{key:"callStatus",value:(u=a(regeneratorRuntime.mark((function e(){var t;return regeneratorRuntime.wrap((function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,fetch(this._basePath+"status",{method:"GET",credentials:"include",headers:this._getHeader()});case 2:return t=e.sent,e.next=5,t.json();case 5:return e.abrupt("return",e.sent);case 6:case"end":return e.stop()}}),e,this)}))),function(){return u.apply(this,arguments)})},{key:"callRestore",value:(s=a(regeneratorRuntime.mark((function e(){var t;return regeneratorRuntime.wrap((function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,fetch(this._basePath+"restore",{method:"POST",credentials:"include",headers:this._getHeader()});case 2:return t=e.sent,e.next=5,t.json();case 5:return e.abrupt("return",e.sent);case 6:case"end":return e.stop()}}),e,this)}))),function(){return s.apply(this,arguments)})},{key:"_getHeader",value:function(){return{Accept:"application/json","Content-Type":"application/json","X-Flow-Csrftoken":this._csrfToken}}}])&&i(t.prototype,n),e}();module.exports=t})();
+
+/***/ }),
+
 /***/ "./node_modules/@neos-project/neos-ui-extensibility/dist/createConsumerApi.js":
 /*!************************************************************************************!*\
   !*** ./node_modules/@neos-project/neos-ui-extensibility/dist/createConsumerApi.js ***!
@@ -725,43 +736,6 @@ exports["default"] = positionalArraySorter;
 
 /***/ }),
 
-/***/ "./node_modules/@unikka/loginas-api/dist sync recursive":
-/*!****************************************************!*\
-  !*** ./node_modules/@unikka/loginas-api/dist sync ***!
-  \****************************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-function webpackEmptyContext(req) {
-	var e = new Error("Cannot find module '" + req + "'");
-	e.code = 'MODULE_NOT_FOUND';
-	throw e;
-}
-webpackEmptyContext.keys = function() { return []; };
-webpackEmptyContext.resolve = webpackEmptyContext;
-module.exports = webpackEmptyContext;
-webpackEmptyContext.id = "./node_modules/@unikka/loginas-api/dist sync recursive";
-
-/***/ }),
-
-/***/ "./node_modules/@unikka/loginas-api/dist/index.js":
-/*!********************************************************!*\
-  !*** ./node_modules/@unikka/loginas-api/dist/index.js ***!
-  \********************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-var require;parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if( true&&"string"==typeof t)return __webpack_require__("./node_modules/@unikka/loginas-api/dist sync recursive")(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]); true?module.exports=l:undefined}if(parcelRequire=f,i)throw i;return f}({"hKzT":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;var e=function(e){return null==e},t=e;exports.default=t;
-},{}],"hPjX":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"isNil",{enumerable:!0,get:function(){return e.default}});var e=r(require("./isNil"));function r(e){return e&&e.__esModule?e:{default:e}}
-},{"./isNil":"hKzT"}],"Focm":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;var e=require("./Helper");function r(e,r,t,n,a,i,s){try{var o=e[i](s),u=o.value}catch(c){return void t(c)}o.done?r(u):Promise.resolve(u).then(n,a)}function t(e){return function(){var t=this,n=arguments;return new Promise(function(a,i){var s=e.apply(t,n);function o(e){r(s,a,i,o,u,"next",e)}function u(e){r(s,a,i,o,u,"throw",e)}o(void 0)})}}function n(e){throw new TypeError('"'+e+'" is read-only')}function a(e,r){if(!(e instanceof r))throw new TypeError("Cannot call a class as a function")}function i(e,r){for(var t=0;t<r.length;t++){var n=r[t];n.enumerable=n.enumerable||!1,n.configurable=!0,"value"in n&&(n.writable=!0),Object.defineProperty(e,n.key,n)}}function s(e,r,t){return r&&i(e.prototype,r),t&&i(e,t),e}var o=function(){function r(t,i){if(a(this,r),(0,e.isNil)(t)){var s="Tried to create API service without a base uri. ";s+=(n("pathError"),"Please initialize the API service with a base path "),s+=(n("pathError"),'like "/neos/impersonate/"'),console.error(s)}if(this._basePath=t,(0,e.isNil)(i)){var o="Tried to create API service without a CSFR ";o+=(n("csrfError"),"token. Please initialize the API service with a token"),console.error(o)}this._csrfToken=i}return s(r,[{key:"callUserChange",value:function(){var e=t(regeneratorRuntime.mark(function e(r){var t,n;return regeneratorRuntime.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return t={user:r,format:"json"},e.next=3,fetch(this._basePath+"user-change",{method:"POST",credentials:"include",headers:this._getHeader(),body:JSON.stringify(t)});case 3:return n=e.sent,e.next=6,n.json();case 6:return e.abrupt("return",e.sent);case 7:case"end":return e.stop()}},e,this)}));return function(r){return e.apply(this,arguments)}}()},{key:"callStatus",value:function(){var e=t(regeneratorRuntime.mark(function e(){var r;return regeneratorRuntime.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,fetch(this._basePath+"status",{method:"GET",credentials:"include",headers:this._getHeader()});case 2:return r=e.sent,e.next=5,r.json();case 5:return e.abrupt("return",e.sent);case 6:case"end":return e.stop()}},e,this)}));return function(){return e.apply(this,arguments)}}()},{key:"callRestore",value:function(){var e=t(regeneratorRuntime.mark(function e(){var r;return regeneratorRuntime.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,fetch(this._basePath+"restore",{method:"POST",credentials:"include",headers:this._getHeader()});case 2:return r=e.sent,e.next=5,r.json();case 5:return e.abrupt("return",e.sent);case 6:case"end":return e.stop()}},e,this)}));return function(){return e.apply(this,arguments)}}()},{key:"_getHeader",value:function(){return{Accept:"application/json","Content-Type":"application/json","X-Flow-Csrftoken":this._csrfToken}}}]),r}();exports.default=o;
-},{"./Helper":"hPjX"}]},{},["Focm"], null)
-//# sourceMappingURL=/index.js.map
-
-/***/ }),
-
 /***/ "./node_modules/tslib/tslib.es6.js":
 /*!*****************************************!*\
   !*** ./node_modules/tslib/tslib.es6.js ***!
@@ -1048,7 +1022,7 @@ var _plowJs = __webpack_require__(/*! plow-js */ "./node_modules/@neos-project/n
 
 var _reactCssThemr = __webpack_require__(/*! @friendsofreactjs/react-css-themr */ "./node_modules/@neos-project/neos-ui-extensibility/src/shims/vendor/react-css-themr/index.js");
 
-var _loginasApi = __webpack_require__(/*! @unikka/loginas-api */ "./node_modules/@unikka/loginas-api/dist/index.js");
+var _loginasApi = __webpack_require__(/*! @unikka/loginas-api */ "../../../node_modules/@unikka/loginas-api/dist/index.js");
 
 var _loginasApi2 = _interopRequireDefault(_loginasApi);
 
@@ -1265,7 +1239,7 @@ var _react = __webpack_require__(/*! react */ "./node_modules/@neos-project/neos
 
 var _react2 = _interopRequireDefault(_react);
 
-var _loginasApi = __webpack_require__(/*! @unikka/loginas-api */ "./node_modules/@unikka/loginas-api/dist/index.js");
+var _loginasApi = __webpack_require__(/*! @unikka/loginas-api */ "../../../node_modules/@unikka/loginas-api/dist/index.js");
 
 var _loginasApi2 = _interopRequireDefault(_loginasApi);
 
@@ -1284,7 +1258,6 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var BASE_PATH = '/neos/impersonate/';
 
 var restoreUser = function restoreUser(i18nRegistry, addFlashMessage) {
-    console.log('restore origin user');
     var csrfToken = document.getElementById('appContainer').dataset.csrfToken;
 
     new _loginasApi2.default(BASE_PATH, csrfToken).callRestore().then(function (data) {
